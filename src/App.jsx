5f82f15dd80f3a8f9764efd8d1fd48b946fa68ce
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-30 16:06:41 +0100

    ADD: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./assets/components/Header"
 
 function App() {
 
   return (
     <>
-      
+      <Header></Header>
     </>
   )
 }
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,5 +1,11 @@
+import style from "./Header.module.css"
+
 export default function Header() {
     return(
-        
+        <>
+        <header>
+            <h1>LEARN WEB DEVELOPMENT </h1>
+        </header>
+        </>
     )
 }
